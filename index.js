fadbd745d80de6d063b7c86552586a70c324f40a
TuringAlan21 <134909148+TuringAlan21@users.noreply.github.com>
2025-03-08 19:16:49 +0400

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const plusBtn = document.getElementById('plus');
 const minusBtn = document.getElementById('minus');
 const divideBtn = document.getElementById('divide');
 const multiplyBtn = document.getElementById('multiply');
+const clearBtn = document.getElementById('clear');
+const inputs = document.querySelectorAll('.input');
 
 class Calculator {
     constructor(num1, num2) {
@@ -47,4 +49,7 @@ plusBtn.addEventListener('click', () => calculator.logSum());
 minusBtn.addEventListener('click', () => calculator.logSub());
 divideBtn.addEventListener('click', () =>  calculator.logDiv());
 multiplyBtn.addEventListener('click', () => calculator.logMul());
-
+clearBtn.addEventListener('click', function(){
+inputs.forEach(input => input.value = '')
+resultDiv.innerHTML = ''
+})
